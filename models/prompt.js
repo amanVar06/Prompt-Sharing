ac6fdac8fc8879621c26bdc4e12e22fffbb5e657
amanVar06 <amanvarshney212@gmail.com>
2023-10-01 13:36:57 +0530

    first commit

---
+++ models/prompt.js
@@ -0,0 +1,21 @@
+import { models, Schema, model } from "mongoose";
+
+const PromptSchema = new Schema({
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: "User", // on to many relationship
+    // one user can create many prompts
+  },
+  prompt: {
+    type: String,
+    required: [true, "Prompt is required"],
+  },
+  tag: {
+    type: String,
+    required: [true, "Tag is required."],
+  },
+});
+
+const Prompt = models.Prompt || model("Prompt", PromptSchema);
+
+export default Prompt;
